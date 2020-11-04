2ef4e2761b19321e967c6b712cc3d0f6af0cad6f
Elrohir <elrohir.rassant@gmail.com>
2020-11-04 15:08:12 +0100

    UO

---
+++ components/Scene.vue
@@ -0,0 +1,40 @@
+<template>
+<div
+  class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center"
+>
+  <div class="fixed inset-0 transition-opacity" @click="handleClose" >
+    <div
+      class="absolute inset-0 bg-gray-500 opacity-75"
+    ></div>
+  </div>
+  <div
+    class="z-10 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all"
+    role="dialog"
+    aria-modal="true"
+    aria-labelledby="modal-headline"
+  >
+    <div class="max-w-screen-xl rounded overflow-hidden shadow-lg">
+      <div class="relative">
+        <iframe width="600" height="300" :src="this.video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    methods: {
+        handleClose() {
+            this.$emit("close");
+        }
+    },
+    props: {
+        video: {
+            type: String,
+            required: true
+        }
+    }}
+</script>
+
+
---
+++ pages/scene.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="p-8 container">
+    <Scene v-if="isShowModal" @close="toggleModal" :video="video" />
+      <div class="grid grid-cols-3   gap-2">
+        <div class="">
+            <img class="border-solid border-2 border-orange-600" @click="toggleModal" @mouseover="getInfo" :id="data.video1" :src="data.videoimg1" alt="" srcset="">
+            <!-- <p class="text-2xl text-center uppercase tracking-wider">Duel entre Gandalf et le Roi Sorcier</p> -->
+        </div>
+      <div>
+        <img class="border-solid border-2 border-gray-600" :src="data.surprise" alt="" srcset="">
+        </div>
+      <div><img class="border-solid border-2 border-orange-600" @click="toggleModal" @mouseover="getInfo" :id="data.video2" :src="data.videoimg2" alt="" srcset=""></div>
+
+    </div>
+      <div class="flex items-center justify-center pt-4">
+          <iframe :src="data.pool" style="width:680px;height:420px;border:0;">Loading poll...</iframe>
+    </div>
+
+    <p class="text-white text-center pt-4 text-4xl">PROCHAIN DUEL DEMAIN</p>
+    
+  </section>
+</template>
+
+<script>
+export default {
+    layout: 'movie',
+    data() {
+        return {
+            data: [],
+            isShowModal: false
+        }
+    },
+    methods: {
+        toggleModal() {
+                this.isShowModal = !this.isShowModal;
+        },
+        getInfo(e) {
+                this.video = e.target.id;
+                console.log(this.data);
+            },
+        },
+    async fetch() {
+      this.data = await this.$http.$get('https://api.npoint.io/808e2e1884f749e97f0d');
+    }
+}
+</script>
+
+<style>
+
+.container {
+  margin: 15px auto !important;
+  max-width: 1280px !important;
+  font-family: 'kenyan coffee rg' !important;
+}
+
+</style>

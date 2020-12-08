86c07274256c97cc70eb15121a1c80fce1da111d
Elrohir <elrohir.rassant@gmail.com>
2020-12-08 14:51:42 +0100

    Update

--- components/Modal.vue
+++ components/Modal.vue
@@ -20,6 +20,12 @@
         <div v-if="data.map" style="top: 0.45em; left: 0.45em" class="absolute bg-gray-700 hover:bg-gray-800 text-white font-bold py-1 px-2 rounded-full">
           Map {{data.map}} Joueurs
         </div>
+        <!-- <div v-if="data.map" style="top: 0.45em; right: 0.45em" class="absolute bg-gray-700 hover:bg-gray-800 text-yellow-600 font-bold py-1 px-2 rounded-full">
+          {{data.nbmatch}}
+          <svg class="inline-block fill-current w-4 h-4 text-yellow-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clip-rule="evenodd" />
+          </svg>
+        </div> -->
       </div>
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">
@@ -130,7 +136,8 @@
         
       </div>
       <div v-else class="px-6 py-4">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Aucun joueur est dans la région !</span>
+        <span v-if="data.name == 'passage infranchissable'" class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"></span>
+        <span v-else class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Aucun joueur est dans la région !</span>
       </div>
     </div>
   </div>

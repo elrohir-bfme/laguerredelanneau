b1ffa13797382bdacd86fb581a5e36ba5d5325a6
elrohir-bfme <53747262+elrohir-bfme@users.noreply.github.com>
2021-01-03 18:24:49 +0100

    up

---
+++ pages/batiments.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container mx-auto">
+        <h1 class="text-white text-2xl text-center">Les Bâtiments</h1>
+        <h2 class="text-gray-500 text-xl text-center">Les batiments ont des charactétistiques différentes</h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

3e554cf2a9a174ed4afaf2fd4496a09c1399baf4
Paul Rassant <paul.rassant@citykomi.com>
2021-01-15 00:04:52 +0100

    gtgt

--- components/Order.vue
+++ components/Order.vue
@@ -18,7 +18,7 @@
 
         <div class="relative inline-block w-full text-gray-700 col-span-2 md:col-span-4">
             <select  class="w-full h-10 pl-3 pr-6 text-base placeholder-gray-600 border rounded-lg appearance-none focus:shadow-outline" placeholder="Regular input" :id="player.name+'arrive'">    
-                <option v-for="region in regions" :value="region.code">{{region.name}}</option>
+                <option v-for="region in orderedRegions" :value="region.code">{{region.name}}</option>
             </select>
             <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
                 <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
@@ -36,12 +36,19 @@ export default {
     },
     props: {
         player: {
-            type: Array,
+            type: Object,
             required: true
         },
         regions: {
             type: Array,
             required: true
+        }
+    },
+    computed: {
+        orderedRegions: function () {
+            return this.regions.slice().sort(function(a, b){
+                return (a.name > b.name) ? 1 : -1;
+            });
         }
     }
 }

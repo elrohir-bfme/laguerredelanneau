59b49f46b7ab8f542ad8d5e6081497d2d93b0156
Paul Rassant <paul.rassant@citykomi.com>
2021-01-23 22:48:35 +0100

    Update

--- pages/chronologie.vue
+++ pages/chronologie.vue
@@ -3,14 +3,18 @@
   <div class="container mx-auto">
     <div class="flex flex-col text-center w-full mb-4">
       <h1 class="sm:text-3xl text-3xl font-medium title-font mb-4 text-gray-300">Chronologie de la Guerre de l'Anneau</h1>
-      <h2 class="lg:w-2/3 mx-auto leading-relaxed text-2xl">Suivez les évènements et les prises de térritoire dans cette lutte incessante !</h2>
+      <h2 class="lg:w-2/3 text-center mx-auto leading-relaxed text-2xl">Suivez les évènements et les prises de territoire dans cette lutte incessante !</h2>
+      <p class="mx-auto mt-8 mb-4 inline text-2xl text-center text-gray-200 ">Carte Vierge</p>
     </div>
-    <div class="flex flex-wrap -m-4">
-        <div class="w-full p-4">
-            <p class="text-2xl text-center text-gray-200">Carte Vierge</p>
-            <img class="mx-auto border-orange-500 border-2" src="https://cdn.discordapp.com/attachments/766584396429262869/796314529490796564/unknown.png">
+    <div class="flex flex-wrap">
+        <img class="mx-auto border-orange-500 border-2" src="https://cdn.discordapp.com/attachments/766584396429262869/796314529490796564/unknown.png">
+        <div class="mx-auto mt-8 mb-4">
+        <p class="text-2xl text-center text-gray-200">Tour de Préparation (10 janvier - 24 janvier)</p>
         </div>
+        <img class="mx-auto border-orange-500 border-2" src="https://cdn.discordapp.com/attachments/766584396429262869/802652389802639426/unknown.png">
     </div>
+
+    
   </div>
 </section>
 </template>

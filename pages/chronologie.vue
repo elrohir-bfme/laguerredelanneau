7ab2f9c29c6460a53deb85f2b8cc39d32e5f866b
Paul Rassant <paul.rassant@citykomi.com>
2021-01-26 21:32:04 +0100

    uu

--- pages/chronologie.vue
+++ pages/chronologie.vue
@@ -16,6 +16,12 @@
         <p class="text-2xl text-center text-gray-200">Tour 1 (25 janvier)</p>
         </div>
         <img class="mx-auto border-orange-500 border-2"  src="https://cdn.discordapp.com/attachments/766584396429262869/803035626018045982/unknown.png" alt="">
+        <div class="mx-auto mt-8 mb-4 w-full">
+          <p class="text-2xl text-center text-gray-200">Annonce de l'Alliance ! 26 Janvier 2021</p>
+        </div><br>
+        <div class="mx-auto mt-8 mb-4">
+         <blockquote class="twitter-tweet" data-lang="fr" data-theme="dark"><p lang="fr" dir="ltr">Si vous n&#39;avez pas suivi l&#39;annonce !<br><br>Information importante pour la Guerre de l&#39;anneau ! <a href="https://t.co/TGurXNT4MM">pic.twitter.com/TGurXNT4MM</a></p>&mdash; Elrohir (@Elrohir_BFME) <a href="https://twitter.com/Elrohir_BFME/status/1354102280883154947?ref_src=twsrc%5Etfw">26 janvier 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
     </div>
 
     

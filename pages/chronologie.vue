8443b28e0d6a7ef0f82ff5274c81faec61b46500
Paul Rassant <elrohir.rassant@gmail.com>
2021-04-06 10:28:07 +0200

    Update tour 12

--- pages/chronologie.vue
+++ pages/chronologie.vue
@@ -65,6 +65,11 @@
         </div>
         <img class="mx-auto border-orange-500 border-2"  src="https://cdn.discordapp.com/attachments/766584396429262869/826376908777979904/tour11.png" alt="">        
         
+        <div class="mx-auto mt-8 mb-4 w-full">
+        <p class="text-2xl text-center text-gray-200">Tour 12 (06 Avril)</p>
+        </div>
+        <img class="mx-auto border-orange-500 border-2"  src="https://cdn.discordapp.com/attachments/766584396429262869/828902172996796447/unknown.png" alt="">        
+        
         
         
         </div>

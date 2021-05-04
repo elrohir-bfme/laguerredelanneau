ab16afcb451152ca1b16c26a524a7b1707ad615f
Paul Rassant <elrohir.rassant@gmail.com>
2021-05-04 09:45:50 +0200

    fez

--- pages/chronologie.vue
+++ pages/chronologie.vue
@@ -85,6 +85,11 @@
         </div>
         <img class="mx-auto border-orange-500 border-2"  src="https://cdn.discordapp.com/attachments/766584396429262869/836372557270876170/unknown.png" alt="">        
         
+        <div class="mx-auto mt-8 mb-4 w-full">
+        <p class="text-2xl text-center text-gray-200">Tour 16 (04 Mai)</p>
+        </div>
+        <img class="mx-auto border-orange-500 border-2"  src="https://cdn.discordapp.com/attachments/766584396429262869/839044976897294376/unknown.png" alt="">        
+        
         
         
         

9a57f54991c28d119bb9ed624adb3c305ee20ad3
Paul Rassant <elrohir.rassant@gmail.com>
2021-05-26 17:16:37 +0200

    Update selection faction ordres

--- pages/wotn/ordres.vue
+++ pages/wotn/ordres.vue
@@ -1,35 +1,23 @@
 <template>
 <div class="container mx-auto">
-    <label class="text-gray-700" for="animals">
-        Animals
-        <select 
-        v-model="selectedFaction"
-        id="factions" 
-        class="block w-52 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500" 
-        name="factions">
-            <option v-for="faction in factions" :key="faction.name">
-                {{faction.name}}
-            </option>
-        </select>
-    </label>
     <div class="container mx-auto px-4 sm:px-8 max-w-3xl">
     <div class="py-8">
         <div class="flex flex-row mb-1 sm:mb-0 justify-between w-full">
-            <h2 class="text-2xl leading-tight">
-                Users
-            </h2>
-            <div class="text-end">
-                <form class="flex w-full max-w-sm space-x-3">
-                    <div class=" relative ">
-                        <input type="text" id="&quot;form-subscribe-Filter" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name"/>
-                        </div>
-                        <button class="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
-                            Filter
-                        </button>
-                    </form>
-                </div>
-            </div>
-            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <label class="text-gray-700">
+                {{ faction }}
+                <select 
+                v-model="selectedFaction"
+                @change="search($event)"
+                id="factions" 
+                class="block w-52 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500" 
+                name="factions">
+                    <option v-for="faction in factions" :key="faction.id" :value="faction.id">
+                        {{faction.name}}
+                    </option>
+                </select>
+            </label>
+        </div>
+        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
                 <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
                     <table class="min-w-full leading-normal">
                         <thead>
@@ -68,7 +56,7 @@
                                 </td>
                                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                     <p class="text-gray-900 whitespace-no-wrap">
-                                        {{ player.name }}
+                                        {{ player.region.name }}
                                     </p>
                                 </td>
                                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -94,36 +82,14 @@
                             
                         </tbody>
                     </table>
-                    <div class="px-5 bg-white py-5 flex flex-col xs:flex-row items-center xs:justify-between">
-                        <div class="flex items-center">
-                            <button type="button" class="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100">
-                                <svg width="9" fill="currentColor" height="8" class="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z">
-                                    </path>
-                                </svg>
-                            </button>
-                            <button type="button" class="w-full px-4 py-2 border-t border-b text-base text-indigo-500 bg-white hover:bg-gray-100 ">
-                                1
-                            </button>
-                            <button type="button" class="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
-                                2
-                            </button>
-                            <button type="button" class="w-full px-4 py-2 border-t border-b text-base text-gray-600 bg-white hover:bg-gray-100">
-                                3
-                            </button>
-                            <button type="button" class="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
-                                4
-                            </button>
-                            <button type="button" class="w-full p-4 border-t border-b border-r text-base  rounded-r-xl text-gray-600 bg-white hover:bg-gray-100">
-                                <svg width="9" fill="currentColor" height="8" class="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z">
-                                    </path>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
                 </div>
             </div>
+        </div>
+        <div class="text-center mb-40 mt-20">
+        <button @click="generateText" class="inline-flex text-base md:text-3xl items-center h-10 px-40 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:outline-none  focus:shadow-outline hover:bg-indigo-800 " id="btncreateFile" v-if="selectedFaction != null">
+            <svg class="w-10 h-10 mr-3 fill-current" viewBox="0 0 20 20"><path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+            <span>Génération</span>
+        </button>
         </div>
     </div>
 </div>
@@ -132,24 +98,80 @@
 <script>
 
 import getFactions from '~/apollo/queries/faction/factions'
-import getPlayers from '~/apollo/queries/player/players'
+import getFaction from '~/apollo/queries/faction/faction'
 
 export default {
+    layout: 'wotn',
     data() {
       return {
+        faction: '',
         factions: [],
-        players: [],
         selectedFaction: null,
+        players: []
       }
     },
     apollo: {
       factions: {
         query: getFactions
       },
-      players: {
-          query: getPlayers
-      }
     },
+    methods: {
+        async search(event) {
+            try {
+                this.faction = event.target.value;
+                const res = await this.$apollo.query({
+                query: getFaction,
+                variables: {
+                    id: event.target.value,
+                },
+                });
+                if (res) {
+                    this.players = res.data.faction.players;
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        },
+            generateText() {
+                // let data = "";
+                // let faction = "";
+                // let playerArray = [];
+                // switch (this.selectedFaction) {
+                //     case 1 : playerArray = this.playerElfe; faction = "elfe"; break;
+                //     case 2 : playerArray = this.playerRohan; faction = "rohan"; break;
+                //     case 3 : playerArray = this.playerGondor; faction = "gondor"; break;
+                //     case 4 : playerArray = this.playerNain; faction = "nain" ;break;
+                //     case 5 : playerArray = this.playerMordor; faction = "mordor"; break;
+                //     case 6 : playerArray = this.playerIsengard; faction = "isengard"; break;
+                //     case 7 : playerArray = this.playerGobelin; faction = "gobelin"; break;
+                //     case 8 : playerArray = this.playerAngmar; faction = "angmar"; break;
+                //     default : console.log("No faction selected");
+                // }
+                // //Get data
+                // playerArray.forEach(element => {
+                //     data += `${element.name} ${element.code} ${document.getElementById(element.name + "arrive").value}\n`;
+                // });
+                // this.addPlayer.forEach(element => {
+                //     data += `spawn ${element.name.replace(/ /g, '_')} ${document.getElementById(element.name + "arrive").value} ${element.faction}\n`;
+                // });
+                    
+                // //convert the text to BLOB
+                
+                const textToBLOB = new Blob(["data"], { type: "text/plain" });
+                const sFileName = `orders${"faction"}.txt`;	   // The file to save the data.
+                let newLink = document.createElement("a");
+                newLink.download = sFileName;
+                if (window.webkitURL != null) {
+                    newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+                } else {
+                    newLink.href = window.URL.createObjectURL(textToBLOB);
+                    newLink.style.display = "none";
+                    document.body.appendChild(newLink);
+                }
+                newLink.click(); 
+                
+            },
+    }
 }
 </script>
 

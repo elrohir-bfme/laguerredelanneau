72a827559f5cafa2c1d68610a56abb237ebb7897
TK <49778@etu.he2b.be>
2021-05-31 12:30:30 +0200

    Avancement page clan

--- pages/arenedarda/clans.vue
+++ pages/arenedarda/clans.vue
@@ -52,7 +52,7 @@
         <div v-for="clan in clans" :key="clan.id" class="p-4 relative mb-14">
             <div class="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2">
                 <img v-if="clan.img" alt="profil" :src="`http://localhost:1337${clan.img.url}`" 
-                class="mx-auto object-cover rounded-lg h-40 w-40  border-2 border-white dark:border-gray-800"/>
+                class="mx-auto object-cover rounded-lg h-40 w-40 dark:border-gray-800"/>
             </div>
             <div class="bg-gradient-to-r from-gray-800 to-gray-900  hover:from-gray-900  hover:to-gray-800 rounded-lg shadow px-8 py-4 pt-24">
                 <div class="text-center">
@@ -60,29 +60,51 @@
                         {{clan.name}}
                     </p>
                     <p class="text-xl text-gray-200 dark:text-gray-200 font-light">
-                        {{clan.chef}}
+                        {{clan.points}}
                     </p>
                     <p class="text-md text-gray-500 w-60 dark:text-gray-400 mx-auto py-4 font-light">
                         Description
                     </p>
+                    <p>
+                        <div class="mx-auto m-2" v-for="player in clan.players" :key="player.name" > 
+                            <div class="rounded-lg uppercase px-2 py-1 text-xs font-bold mr-3 text-white text-center"
+                                :class="`bg-indigo-900 hover:bg-purple-700`"
+                                >
+
+                                {{(player.name)}}
+                                <span class="block text-center text-xs font-normal">
+
+                                <img v-if="player.grade == 'general'"  class="w-8 h-4 inline-block" src="~assets/wars/gradeGeneral.svg">
+                                <img v-if="player.grade == 'lieutenant'"  class="w-8 h-4 inline-block" src="~assets/wars/gradeLieutenant.svg">  
+                                <img v-if="player.grade == 'soldat'"  class="w-8 h-4 inline-block" src="~assets/wars/gradeSoldat.svg">  
+                                
+                                {{player.victory}} <img alt="Victoire" class="w-8 h-4 inline-block" src="~assets/trophy.svg">
+                                
+                                {{player.defeat}} <img alt="Défaite" class="w-8 h-4 inline-block" src="~assets/skull.svg">
+                            
+                                </span>
+                            </div>
+                        </div>
+                    </p>
+
                 </div>
                 <div class="rounded-lg bg-gray-900 dark:bg-white p-2 w-full">
-                      <div class="flex items-center justify-between text-3xs text-white dark:text-black">
+                      <div class="flex items-center justify-between text-3xs text-white dark:text-white">
                           <p class="flex flex-col">
                               Victoire
-                              <span class="text-black dark:text-indigo-500 font-bold">
+                              <span class="text-white dark:text-indigo-500 font-bold">
                                   {{ clan.win}}
                               </span>
                           </p>
                           <p class="flex flex-col">
                               Défaite
-                              <span class="text-black dark:text-indigo-500 font-bold">
+                              <span class="text-white dark:text-indigo-500 font-bold">
                                   {{ clan.defeat}}
                               </span>
                           </p>
                           <p class="flex flex-col">
                               Matchs
-                              <span class="text-black dark:text-indigo-500 font-bold">
+                              <span class="text-white dark:text-indigo-500 font-bold">
                                   {{ clan.matchs}}
                               </span>
                           </p>

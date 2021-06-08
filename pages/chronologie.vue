9a51db1d209d51ab3ac2b8565a22415cfeb1366b
Paul Rassant <elrohir.rassant@gmail.com>
2021-06-08 13:22:52 +0200

    update

--- pages/chronologie.vue
+++ pages/chronologie.vue
@@ -111,6 +111,11 @@
         </div>
         <img class="mx-auto border-orange-500 border-2"  src="https://cdn.discordapp.com/attachments/766584396429262869/849246152976891904/unknown.png" alt="">        
         
+        <div class="mx-auto mt-8 mb-4 w-full">
+        <p class="text-2xl text-center text-gray-200">Tour 21 (8 Juin)</p>
+        </div>
+        <img class="mx-auto border-orange-500 border-2"  src="https://cdn.discordapp.com/attachments/766584396429262869/851783101897637978/unknown.png" alt="">        
+        
         
         
         

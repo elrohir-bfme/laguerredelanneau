eb2d15646ce99a0cbc8abd007b75cd003c6acae0
elrohir-bfme <53747262+elrohir-bfme@users.noreply.github.com>
2021-06-17 07:27:39 +0200

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -53,9 +53,6 @@
               <br>
               <button style="font-family: 'Albertus'" to="/wotn" 
               class="inline-flex items-center px-2 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-600 rounded-md dark:bg-gray-800 hover:bg-indigo-500 dark:hover:bg-gray-700 focus:outline-none focus:bg-indigo-500 dark:focus:bg-gray-700">
-                <!-- <svg class="w-5 h-5 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg> -->
                 <span class="mx-1">Prochaine évènement</span>
               </button>
             </div>

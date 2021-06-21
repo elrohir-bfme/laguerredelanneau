d7495d1aa8cca2204ec799681cabfec7c4db098c
TK <49778@etu.he2b.be>
2021-06-21 13:00:57 +0200

    Oui

--- pages/arenedarda/index.vue
+++ pages/arenedarda/index.vue
@@ -2,10 +2,10 @@
   <div class="container border-4 border-white bg-gradient-to-r from-gray-600 via-gray-700 to-gray-900  hover:from-gray-900 hover:via-gray-700  hover:to-gray-600 rounded-lg mx-auto w-4/5 relative flex items-center py-16 px-4 m-4">
     <div class="w-full flex flex-col items-center justify-between relative z-10">
         <h1 class="flex flex-col items-center font-extrabold text-3xl text-center md:text-6xl text-white">
-            Bienvenue dans l'Arêne de la Terre du Milieu ⚔️
+            Bienvenue dans l'Arène d'Arda ⚔️
         </h1>
         <h2 class="flex flex-col max-w-lg text-center items-center font-extrabold text-lg mt-6 text-white">
-            Créer votre clan de 6 guerriers et affrontez les autres clans pour devenir l'équipe la plus puissante de la Terre du Milieu
+            Créer votre maison de 6 à 10 guerriers et affrontez les autres maison pour devenir le numéro 1 d'Arda
         </h2>
         <a href="http://laterredumilieu.fr/Discord" class="block bg-gray-800 hover:bg-gray-900 py-3 px-4 text-lg text-white font-bold uppercase mt-10">
             Inscription sur le Discord

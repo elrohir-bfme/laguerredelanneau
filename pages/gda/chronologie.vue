5f3048365688e29ea8e2c177ea1b8ea4225a680f
elrohir-bfme <53747262+elrohir-bfme@users.noreply.github.com>
2021-07-30 09:11:41 +0200

    Update tour 32

--- pages/gda/chronologie.vue
+++ pages/gda/chronologie.vue
@@ -166,6 +166,11 @@
         </div>
         <img class="mx-auto border-orange-500 border-2"  src="https://cdn.discordapp.com/attachments/766584396429262869/870197776577888256/unknown.png" alt="">        
         
+        <div class="mx-auto mt-8 mb-4 w-full">
+        <p class="text-2xl text-center text-gray-200">Tour 32 (30 Juillet)</p>
+        </div>
+        <img class="mx-auto border-orange-500 border-2"  src="https://cdn.discordapp.com/attachments/766584396429262869/870563312071475221/unknown.png" alt="">        
+        
         
         
         </div>

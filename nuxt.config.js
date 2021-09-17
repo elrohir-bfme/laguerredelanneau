0455a81759064f9b7adbcfbff659b957bebcd271
elrohir-bfme <53747262+elrohir-bfme@users.noreply.github.com>
2021-09-17 08:36:57 +0200

    Update try seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,12 +21,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        hid: "canonical",
-        rel: "canonical",
-        href: "https://laguerredelanneau.vercel.app",
-      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*

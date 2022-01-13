3aec2c6ae4a5890be3acb29bada805fd4dbf8642
elrohir-bfme <elrohir.rassant@gmail.com>
2022-01-13 13:52:39 +0100

    Update

---
+++ components/Timer.vue
@@ -0,0 +1,68 @@
+<template>
+    <span class="mx-1">Bande annonce le : {{ days }} jours {{ hours }} heures {{ minutes }} minutes {{ seconds }} secondes</span>
+</template>
+
+<script>
+export default {
+  data() {
+  	return{
+      endtime:"Jan 15, 2022 18:00:00",
+      wordString: {},
+      start: "",
+      end: "",
+      interval: "",
+      days:"",
+      minutes:"",
+      hours:"",
+      seconds:"",
+      message:"",
+      statusType:"",
+      statusText: "",
+    
+    };
+  },
+  mounted(){
+    this.start = new Date().getTime();
+    this.end = new Date(this.endtime).getTime();
+    // Update the count down every 1 second
+    this.timerCount(this.start,this.end);
+    this.interval = setInterval(() => {
+        this.timerCount(this.start,this.end);
+    }, 1000);
+  },
+  methods: {
+    timerCount(start,end){
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        var distance = start - now;
+        var passTime =  end - now;
+
+        if(distance < 0 && passTime < 0){
+            this.message = this.wordString.expired;
+            this.statusType = "expired";
+            clearInterval(this.interval);
+            return;
+
+        }else if(distance < 0 && passTime > 0){
+            this.calcTime(passTime);
+            this.message = this.wordString.running;
+            this.statusType = "running";
+
+        } else if( distance > 0 && passTime > 0 ){
+            this.calcTime(distance); 
+            this.message = this.wordString.upcoming;
+            this.statusType = "upcoming";
+        }
+    },
+    calcTime(dist){
+      // Time calculations for days, hours, minutes and seconds
+        this.days = Math.floor(dist / (1000 * 60 * 60 * 24));
+        this.hours = Math.floor((dist % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        this.minutes = Math.floor((dist % (1000 * 60 * 60)) / (1000 * 60));
+        this.seconds = Math.floor((dist % (1000 * 60)) / 1000);
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -46,7 +46,8 @@
               <br>
               <button
               class="inline-flex items-center px-2 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-600 rounded-md dark:bg-gray-800 hover:bg-indigo-500 dark:hover:bg-gray-700 focus:outline-none focus:bg-indigo-500 dark:focus:bg-gray-700">
-                <span class="mx-1">Prochain évènement</span>
+                
+                <Timer/>
               </button>
             </div>
           </nuxt-link>

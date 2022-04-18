a27883b4b319ed70c85410a5c2fdb323089dd507
elrohir-bfme <elrohir.rassant@gmail.com>
2022-04-19 00:10:03 +0200

    Update deroulement

---
+++ pages/loup-garou/deroulement.vue
@@ -0,0 +1,281 @@
+<template>
+  <section class="text-indigo-400 body-font overflow-hidden">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="-my-8 divide-y-2 divide-purple-400">
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Noctambule</span>
+          <span class="mt-1 text-purple-600 text-sm">Numéro 1</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Sam</h2>
+          <p class="leading-relaxed">il faut quand même par la suite appeler le rôle désactiver pour pas mettre au courant le village sur le rôle qui n’as pas pu jouer pendant la nuit</p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">pirate</span>
+          <span class="mt-1 text-purple-600 text-sm">Numéro 2</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Peter Jackson</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Enfant sauvage</span>
+          <span class="text-sm text-purple-600">Numéro 3</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Marteau de Helm</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Chien loup</span>
+          <span class="text-sm text-purple-600">Numéro 4</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Isildur</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Les deux frères</span>
+          <span class="text-sm text-purple-600">Numéro 5</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Elladan et Elrohir</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Cupidon</span>
+          <span class="text-sm text-purple-600">Numéro 6</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Arwen</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500"></span>
+          <span class="text-sm text-purple-600">Numéro 7</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Le Couple</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Voyante</span>
+          <span class="text-sm text-purple-600">Numéro 8</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Galadriel</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Renard</span>
+          <span class="text-sm text-purple-600">Numéro 9</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Faramir</h2>
+          <p class="leading-relaxed">S’il n’a pas perdu son pouvoir la nuit d’avant en découvrant 3 innocents avec son pouvoir</p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Salvateur</span>
+          <span class="text-sm text-purple-600">Numéro 10</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Gimli</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Petite fille</span>
+          <span class="text-sm text-purple-600">Numéro 11</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Frodon</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Survivant</span>
+          <span class="text-sm text-purple-600">Numéro 12</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Gollum</h2>
+          <p class="leading-relaxed">Même s'il n'a plus de cachette</p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Loup Garou</span>
+          <span class="text-sm text-purple-600">Numéro 13</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Nazguls</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Loup noir</span>
+          <span class="text-sm text-purple-600">Numéro 14</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Sauron</h2>
+          <p class="leading-relaxed">Uniquement si aucun loup n'est mort</p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Infecte père des loups</span>
+          <span class="text-sm text-purple-600">Numéro 15</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Roi sorcier</h2>
+          <p class="leading-relaxed">Même s'il a déjà utilisé son pouvoir de ne pas révéler au village si quelqu'un a été infecté</p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Loup blanc</span>
+          <span class="text-sm text-purple-600">Numéro 16</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Saruman</h2>
+          <p class="leading-relaxed">Uniquement kirs de la 2e/4e/6e/8e nuit etc...</p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Sorcière</span>
+          <span class="text-sm text-purple-600">Numéro 17</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Mage bleu</h2>
+          <p class="leading-relaxed">Même si elle a déjà utilisé ses 2 potions pour pas montrer qu'elle n'en a plus</p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Joeur de flute</span>
+          <span class="text-sm text-purple-600">Numéro 18</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Arachné</h2>
+          <p class="leading-relaxed">Il faut ensuite prévenir les joueurs ciblés</p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Assassin</span>
+          <span class="text-sm text-purple-600">Numéro 19</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Grima</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Pyromane</span>
+          <span class="text-sm text-purple-600">Numéro 20</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Smaug</h2>
+          <p class="leading-relaxed">Il faut ensuite prévenir le joueurs ciblés</p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Dictateur</span>
+          <span class="text-sm text-purple-600">Numéro 21</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Denethor</h2>
+          <p class="leading-relaxed">Uniquement s'il n'a pas utilisé son pouvoir</p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Détective</span>
+          <span class="text-sm text-purple-600">Numéro 22</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Aragorn</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Chaman</span>
+          <span class="text-sm text-purple-600">Numéro 23</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Tom Bombadil</h2>
+          <p class="leading-relaxed">A partir de la 2ème nuit quand ils a des morts</p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Prêtre</span>
+          <span class="text-sm text-purple-600">Numéro 24</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Manwe</h2>
+          <p class="leading-relaxed">A partir de la 2ème nuit vue qu'il y'a pas encore de mort annoncé lors de la 1ère</p>
+        </div>
+      </div>
+
+      <div class="p-4 flex flex-wrap md:flex-nowrap bg-gray-900">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-purple-500">Voleur</span>
+          <span class="text-sm text-purple-600">Numéro 25</span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="text-2xl font-medium text-indigo-500 title-font mb-2">Bilbo</h2>
+          <p class="leading-relaxed"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+    layout: "loup-garou"
+}
+</script>
+
+<style>
+
+</style>

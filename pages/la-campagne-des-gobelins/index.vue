c9d95e8b72ec9fb277d122cfd9468630d5c044df
elrohir-bfme <elrohir.rassant@gmail.com>
2022-08-29 22:07:15 +0200

    up text

--- pages/la-campagne-des-gobelins/index.vue
+++ pages/la-campagne-des-gobelins/index.vue
@@ -19,35 +19,61 @@
           </div>
         </div>
         <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-indigo-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-          <p class="leading-relaxed text-xl mb-4 text-white">La Campagne des Gobelins est une nouvelle forme d’événement pour le STR BFME2, L'Avènement du Roi Sorcier. Basée sur des mécaniques empruntées à la fois aux jeux de plateau et aux jeux de cartes, la Campagne se veut un outil ludique permettant de modifier en profondeur le Gamplay des joueurs, et ainsi avoir l'impression de jouer à un tout nouveau jeu ! La Campagne fera également intervenir une grande part de hasard par le biais de tirage au sort discriminant les différents joueurs qui participeront aux Combats.
-Deux équipes vont s’affronter durant plusieurs semaines (5 sem. min., 10 sem. max.) pour tenter de remporter un maximum de Points de Campagne. Pour y arriver, les cartes à jouer donneront un avantage certain à leurs possesseurs... pour peu qu'elles soient judicieusement jouées !</p>
+          <p class="leading-relaxed text-xl mb-4 text-white">La Campagne des Gobelins est une nouvelle forme d’événement pour le STR La Bataille pour la Terre du Milieu 2, L'Avènement du Roi-Sorcier. 
+            <br/>
+            Basée sur des mécaniques empruntées à la fois aux jeux de plateau et aux jeux de cartes, la Campagne se veut un outil ludique permettant de modifier en profondeur le gameplay des joueurs, de base obligés d’incarner la faction gobeline, et ainsi avoir l'impression de jouer à un tout nouveau jeu ! 
+            <br/>
+            Le jeu fait également intervenir une grande part de hasard par le biais de tirages au sort discriminant, entre autres, les différents joueurs qui participeront aux Combats.
+            <br/>
+            Deux équipes vont s’affronter durant plusieurs semaines pour tenter de remporter un maximum de Points de Campagne. Pour y arriver, les cartes à jouer donneront un avantage certain à leurs possesseurs... pour peu qu'elles soient judicieusement jouées ! 
+            <br/>
+            Bonne découverte et bon amusement à tous ! 
+            
+          </p>
         </div>
       </div>
       <div class="flex flex-col xl:flex-row mt-10">
         <div class="xl:w-2/3 sm:pr-8 sm:py-8 sm:border-r border-indigo-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-right">
           <p class="leading-relaxed text-xl mb-4 text-white">
           Scénario : La Guerre des Slips
-
-          Voilà bien longtemps que le Mont Quasi-Perdu est occupé par la race gobeline. Au fil des siècles, les passages étroits à travers la roche ont été retaillés pour faire place à des galeries sinueuses et les parois des cavités naturelles furent élargies pour devenir de grandes et larges salles. Les Gobelins, contents de leur ouvrage, passaient leur vie à ripailler et à s’en prendre à toute âme assez sotte pour s’aventurer à l'ombre de leur relief. 
-          Mais le petit monde du Mont Quasi-Perdu est sur le point de changer… Car le temps est venu pour les Gobelins de régler leurs comptes…
-          Voilà bien longtemps qu’un Gobelin, un certain Morko, voleur de slips ambulant, s’adonne à son passe-temps favori sans jamais avoir été inquiété. Pourtant, nombreux sont ceux parmi les Gobelins qui, du jour au lendemain, ont dû se résigner à faire traîner leurs précieux atours à même la roche. Les fautives – pensaient-ils – ne pouvaient être que les Termites des Tunnels qui, une fois le temps de se reposer arrivé, devaient profiter de l’instant pour venir grignoter pagnes et slips qui traînaient par-ci, par-là. 
+          <br/><br/>
+          Voilà bien longtemps que le Mont Quasi-Perdu est occupé par la race gobeline. Au fil des siècles, les passages étroits à travers la roche ont été retaillés pour faire place à des galeries sinueuses et les parois des cavités naturelles furent élargies pour devenir de grandes et larges salles. Les Gobelins, satisfaits de leur ouvrage, passaient leur vie à ripailler et à s’en prendre à toute âme assez sotte pour s’aventurer à l'ombre de leur relief. Mais le petit monde du Mont Quasi-Perdu est sur le point de changer, car le temps est venu pour les Gobelins de régler leurs comptes…
+          <br/>
+          <br/>
+          Durant plusieurs années, le Gobelin Morko s’est adonné à son forfait favori - le vol de slips - sans jamais avoir été inquiété.
+          <br/>
+          Pourtant, nombreux sont ceux parmi les Gobelins qui, du jour au lendemain, ont dû se résigner à faire traîner leurs précieux atours à même la roche. Les fautives – pensaient-ils – ne pouvaient être que les Termites des Tunnels qui, une fois les Gobelins assoupis, devaient profiter de l’instant pour venir grignoter pagnes et slips qui traînaient par-ci, par-là.
           </p>
         </div>
         <div class="xl:w-1/3 text-center sm:pl-8 sm:py-8">
-          <img src="~assets/campagne_gobelins/TRA_decret_royal.jpg" class="ease-in-out transform transition duration-500 hover:scale-110 hover:drop-shadow-xl h-full rounded-lg inline-flex items-center justify-center bg-gray-200 text-gray-400">
+          <img src="~assets/campagne_gobelins/logo.png" class="inline-flex items-center justify-center">
         </div>
       </div>
       <div class="flex flex-col sm:flex-row mt-10">
         <div class="sm:w-3/3 sm:py-8 mt-4 pt-4 sm:mt-0 text-center sm:text-left">
           <p class="leading-relaxed text-xl mb-4 text-white">
-            Mais la disparition du slip d’Utharlok Foudre-Allumée, un gobelin ermite, vieux, sage, et pourtant parfois très con, a fait jaillir un nouveau débat. Les Gobelins furent tout d’abord hallucinés par cette vue hors du commun qui s’offrait à eux. Ensuite, ils se mirent à réfléchir… et à jacasser en tous sens... 
-            Sont-ce vraiment ces fichues Termites qui mangent nos slips ? Si c’était le cas, Utharlok aurait dû s’en prévenir, habitué qu’il est à leur présence ? 
-            C’est alors qu’un certain Gobelin s’interposa dans la cohue : Ruik le Tisseur. Ce Gobelin à l’esprit vif désigna du doigt Morko et prononça ces paroles : 
-            Vos slips n’ont jamais été dévorés par les Termites ! Morko, qu’il est de bon ton de désormais appeler Morko L’Arrache-Slip, vous les a volés ! Kyagara Yeux-Furtifs l’a vu ! 
-            De nombreux Gobelins, comprenant que leur nudité n’était pas le forfait d’un quelconque insecte et qu’ils avaient été spoliés, partirent dans une rage folle. Mais d’autres, trouvant la situation particulièrement hilarante, se mirent à rire à gorge déployée. Des rixes éclatèrent dans les galeries, et deux Clans se formèrent alors :  d’un côté, les Gobelins sans Slip, vexés de leur nouvelle situation. De l’autre, les Gobelins en Slip, perpétuellement morts de rire. Le conflit se généralisa de telle sorte qu’Azog et Gorkil, les deux figures de proue de ce petit monde, finirent par prendre parti. 
+            Mais la disparition du slip d’Utharlok Foudre-Allumée, un vieux Gobelin ermite, a fait jaillir un nouveau débat. Les Gobelins furent tout d’abord hallucinés par cette vue hors du commun qui s’offrait à eux. Ensuite, ils se mirent à réfléchir… et à jacasser en tous sens...
+            <br/>
+            <br/>
+            « Sont-ce vraiment ces fichues Termites qui mangent nos slips ? Si c’était le cas, Utharlok aurait dû s’en prévenir, habitué qu’il est à leur présence ? » 
+            -
+            <br/>
+            C’est alors qu’un certain Gobelin s’interposa dans la cohue : Ruik le Tisseur. Celui-là avait l’esprit vif et désigna du doigt Morko en prononçant ces paroles : 
+            <br/>
+            <br/>
+            « Vos slips n’ont jamais été dévorés par les Termites ! Morko, qu’il est de bon ton de désormais appeler Morko L’Arrache-Slip, vous les a volés ! Kyagara Yeux-Furtifs l’a vu ! »
+            <br/>
+            De nombreux Gobelins, comprenant que leur nudité n’était pas le forfait d’un quelconque insecte mais qu’ils avaient été spoliés, partirent dans une rage folle. 
+            <br/>
+            <br/>
+            Mais d’autres, trouvant la situation particulièrement hilarante, se mirent à rire à gorge déployée. 
+            <br/>
+            Des rixes éclatèrent dans les galeries, et deux Clans se formèrent alors :  d’un côté, les Gobelins sans Slip, vexés de leur nouvelle situation. De l’autre, les Gobelins en Slip, perpétuellement morts de rire. 
+            <br/>
+            Le conflit se généralisa de telle sorte qu’Azog et Gorkil, les deux figures de proue de ce petit monde, finirent par prendre parti. 
+            <br/><br/>
+            La Grande Guerre des Slips pouvait commencer… 
 
-            <br><br>
-            La grande Guerre des Slips pouvait commencer…
           </p>
         </div>
       </div>

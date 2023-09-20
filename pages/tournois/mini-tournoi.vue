10b8150bcccac91b1d948af93ebe4d2d9e8ddef6
elrohir-bfme <elrohir.rassant@gmail.com>
2023-09-20 20:29:29 +0200

    up

--- pages/tournois/mini-tournoi.vue
+++ pages/tournois/mini-tournoi.vue
@@ -1,9 +1,9 @@
 <template>
 <div>
-  <div class="bg-gray-900 dark:bg-gray-800 ">
+  <div>
     <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
         <h1 class="text-3xl font-extrabold text-gray-200 dark:text-white sm:text-4xl">
-          Les Tournois du Week-end (TWE)
+          Les Mini Tournois et Semaine
         </h1>
         <h2 class="text-2xl font-extrabold text-orange-500 dark:text-white sm:text-3xl">
             Liste des mini-tournois de la Terre du Milieu
@@ -18,91 +18,35 @@
 
     </div>
 
-    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4">
-      <NuxtImg
-        src="tournois/weekend/1.png"
+    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4 bg-gray-900">
+      <img
+        src="https://cdn.discordapp.com/attachments/1150777936635506718/1154026437456699543/image.png"
         class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
         alt="Tournoi weekend"
         loading="lazy"
       />
       <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
-        <p class="text-sm title-font text-white tracking-widest">📅 7 et 8 janvier 2023</p>
-        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Première édition du TWE</p>
-        <p class="leading-relaxed text-orange-500">🏆 remporté par SOLID | Aupif</p>
+        <p class="text-sm title-font text-white tracking-widest">📅  début 20 septembre 2023</p>
+        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Neuvième édition - TDS</p>
+        <p class="leading-relaxed text-orange-500">En cours</p>
       </div>
     </div>
 
-    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4">
+    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4 bg-gray-900">
       <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
-        <p class="text-sm title-font text-white tracking-widest">📅 14 et 15 janvier (+ 16janvier) 2023</p>
-        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Seconde édition du TWE</p>
-        <p class="leading-relaxed text-orange-500">🏆 remporté par Diablo</p>
+        <p class="text-sm title-font text-white tracking-widest">📅  14 au 18 septembre 2023</p>
+        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Huitième édition - Tournoi semaine</p>
+        <p class="leading-relaxed text-orange-500">🏆 remporté par Gimli fils de Gloin</p>
       </div>
       <NuxtImg
-        src="tournois/weekend/2.png"
-        class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
-        alt="Tournoi weekend"
-        loading="lazy"
-      />
-    </div>
-
-    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4">
-      <NuxtImg
-        src="tournois/weekend/3.png"
-        class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
-        alt="Tournoi weekend"
-        loading="lazy"
-      />
-      <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
-        <p class="text-sm title-font text-white tracking-widest">📅  21 et 22 janvier 2023</p>
-        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Troisième édition du TWE</p>
-        <p class="leading-relaxed text-orange-500">🏆 remporté par Thieuthieu De Rune, Vala du Sel (par abandon)</p>
-      </div>
-    </div>
-
-    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4">
-      <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
-        <p class="text-sm title-font text-white tracking-widest">📅  28 et 29 janvier 2023</p>
-        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Quatrième édition du TWE</p>
-        <p class="leading-relaxed text-orange-500">🏆 remporté par Kyagara</p>
-      </div>
-      <NuxtImg
-        src="tournois/weekend/4.png"
+        src="tournois/weekend/7.png"
         class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
         alt="Tournoi weekend"
         loading="lazy"
       />
     </div>
 
-    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4">
-      <NuxtImg
-        src="tournois/weekend/5.png"
-        class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
-        alt="Tournoi weekend"
-        loading="lazy"
-      />
-      <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
-        <p class="text-sm title-font text-white tracking-widest">📅  4 et 5 février 2023</p>
-        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Cinquième édition du TWE</p>
-        <p class="leading-relaxed text-orange-500">🏆 remporté par Diablo</p>
-      </div>
-    </div>
-
-    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4">
-      <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
-        <p class="text-sm title-font text-white tracking-widest">📅  17 au 19 février 2023</p>
-        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Sixième édition du TWE</p>
-        <p class="leading-relaxed text-orange-500">🏆 remporté par Diablo</p>
-      </div>
-      <NuxtImg
-        src="tournois/weekend/6.png"
-        class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
-        alt="Tournoi weekend"
-        loading="lazy"
-      />
-    </div>
-
-    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4">
+    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4 bg-gray-900">
       <NuxtImg
         src="tournois/weekend/7.png"
         class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
@@ -116,6 +60,99 @@
       </div>
     </div>
 
+    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4 bg-gray-900">
+      <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
+        <p class="text-sm title-font text-white tracking-widest">📅  17 au 19 février 2023</p>
+        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Sixième édition du TWE</p>
+        <p class="leading-relaxed text-orange-500">🏆 remporté par Diablo</p>
+      </div>
+      <NuxtImg
+        src="tournois/weekend/6.png"
+        class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
+        alt="Tournoi weekend"
+        loading="lazy"
+      />
+    </div>
+
+    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4 bg-gray-900">
+      <NuxtImg
+        src="tournois/weekend/5.png"
+        class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
+        alt="Tournoi weekend"
+        loading="lazy"
+      />
+      <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
+        <p class="text-sm title-font text-white tracking-widest">📅  4 et 5 février 2023</p>
+        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Cinquième édition du TWE</p>
+        <p class="leading-relaxed text-orange-500">🏆 remporté par Diablo</p>
+      </div>
+    </div>
+
+    
+
+
+
+    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4 bg-gray-900">
+      <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
+        <p class="text-sm title-font text-white tracking-widest">📅  28 et 29 janvier 2023</p>
+        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Quatrième édition du TWE</p>
+        <p class="leading-relaxed text-orange-500">🏆 remporté par Kyagara</p>
+      </div>
+      <NuxtImg
+        src="tournois/weekend/4.png"
+        class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
+        alt="Tournoi weekend"
+        loading="lazy"
+      />
+    </div>
+
+
+    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4 bg-gray-900">
+      <NuxtImg
+        src="tournois/weekend/3.png"
+        class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
+        alt="Tournoi weekend"
+        loading="lazy"
+      />
+      <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
+        <p class="text-sm title-font text-white tracking-widest">📅  21 et 22 janvier 2023</p>
+        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Troisième édition du TWE</p>
+        <p class="leading-relaxed text-orange-500">🏆 remporté par Thieuthieu De Rune, Vala du Sel (par abandon)</p>
+      </div>
+    </div>
+
+
+    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4 bg-gray-900">
+      <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
+        <p class="text-sm title-font text-white tracking-widest">📅 14 et 15 janvier (+ 16janvier) 2023</p>
+        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Seconde édition du TWE</p>
+        <p class="leading-relaxed text-orange-500">🏆 remporté par Diablo</p>
+      </div>
+      <NuxtImg
+        src="tournois/weekend/2.png"
+        class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
+        alt="Tournoi weekend"
+        loading="lazy"
+      />
+    </div>
+    
+
+    <div class="lg:w-4/5 mx-auto flex flex-wrap border-2 border-orange-500 mb-4 bg-gray-900">
+      <NuxtImg
+        src="tournois/weekend/1.png"
+        class="lg:w-3/4 w-full lg:h-auto h-64 object-cover object-center rounded p-4"
+        alt="Tournoi weekend"
+        loading="lazy"
+      />
+      <div class="lg:w-1/4 w-full lg:pl-10 lg:py-6 lg:px-4 mt-6 lg:mt-0 text-center items-center self-center">
+        <p class="text-sm title-font text-white tracking-widest">📅 7 et 8 janvier 2023</p>
+        <p class="text-gray-200 text-3xl title-font font-medium mb-1">Première édition du TWE</p>
+        <p class="leading-relaxed text-orange-500">🏆 remporté par SOLID | Aupif</p>
+      </div>
+    </div>
+
+    
+
 </div>
 </div>
 </template>
@@ -125,7 +162,7 @@ export default {
   layout: "home",
   head() {
     return {
-      title: "Les Tournois du Week-end (TWE)",
+      title: "Les Mini Tournois",
       meta: [
         {
           hid: 'description',
@@ -147,7 +184,7 @@ export default {
         {
           hid: 'twitter:title',
           name: 'twitter:title',
-          content: "Les Tournois du Week-end (TWE)",
+          content: "Les Mini Tournois",
         },
         {
           hid: 'twitter:description',
@@ -166,7 +203,7 @@ export default {
           content:
             'https://laguerredelanneau.vercel.app/tournois/weekend/tournoi.jpg',
         },
-        { hid: 'og:title', name: 'og:title', content: "Les Tournois du Week-end (TWE)" },
+        { hid: 'og:title', name: 'og:title', content: "Les Mini Tournois" },
         { hid: 'og:type', name: 'og:type', content: 'website' },
         {
           hid: 'og:url',
